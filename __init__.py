57c2be63a816dd331e913fab9faca76b84dabcf2
4onenm <blueridgemax@gmail.com>
2022-03-22 10:26:02 -0700

    Add Scene Select support to Bryce3

--- __init__.py
+++ __init__.py
@@ -311,11 +311,25 @@ def add_scene_select():
     fss.register_scene_select_cateogry(bangok, nsfw=True)
 
     fss.register_scene_select(bangok, "Drunk with Bryce", 'bangok_four_bryce1_apartment_decided', replay_scope=fss.extend_scope(
-                        bangok_four_malepartners = 0,
-                        bangok_four_femalepartners = 0,
-                        bangok_four_playerhasdick = None,
+                        # bangok_four_malepartners = 0,
+                        # bangok_four_femalepartners = 0,
+                        # bangok_four_playerhasdick = None,
                         brycemood = 4,))
 
+    fss.register_scene_select(bangok, "Bryce and Maverick at the BBQ", 'bangok_four_bryce3_replaylabel',
+                                locked=lambda: not renpy.store.persistent.bryce3skip,
+                                replay_scope=fss.extend_scope_by_dict({
+                                    'bangok_four_malepartners': 1,
+                                }))
+
+    fss.register_scene_select(bangok, "Bryce, Maverick, and Sebastian at the BBQ", 'bangok_four_bryce3_replaylabel',
+                                locked=lambda: not renpy.store.persistent.bryce3skip,
+                                replay_scope=fss.extend_scope_by_dict({
+                                    'bangok_four_malepartners': 1,
+                                    'bangok_four_bryce1_unplayed': False,
+                                    'bangok_four_xsebastian_unplayed': False,
+                                    'bangok_four_bryce3_store.sebastian_in': True,
+                                }))
 
 @loadable_mod
 class BangOkMod(Mod):
